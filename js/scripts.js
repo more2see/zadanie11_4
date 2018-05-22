ed757c773df705fb09db0a314f5f0f5eab1589c5
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-22 12:58:26 +0200

    Add scripts to scripts.js  with discounted price for a randomModel

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,24 @@ function Phone(brand, price, color, quality) {
 	this.quality = quality;
 }
 var iPhone6S = new Phone("Apple", 2250, "silver", "new");
-var SamsungGalaxyS6 = new Phone("Samsung", 100, "black", "old");
-var OnePlusOne = new Phone("One", 350, "white", "old");
-var Nokia = new Phone("First", 230, "blue", "new");
+var samsungGalaxyS6 = new Phone("Samsung", 100, "black", "old");
+var onePlusOne = new Phone("One", 350, "white", "old");
+var nokia = new Phone("First", 230, "blue", "new");
 Phone.prototype.printInfo = function() {
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + " and quality is " + this.quality + ".");
 };
 iPhone6S.printInfo();
-SamsungGalaxyS6.printInfo();
-OnePlusOne.printInfo();
-Nokia.printInfo();
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
+nokia.printInfo();
+
+/*Działa ale dla nowych zmiennych i paramentów */
+function ApplyDiscount(normalPrice, discount) {
+	this.normalPrice = normalPrice;
+	this.discount = discount;
+}
+var randomModel = new ApplyDiscount(200, 50);
+ApplyDiscount.prototype.printInfo = function() {
+	console.log("The phone randomModel price is " + this.normalPrice +  ", the discounted price is " + ( this.discount * this.normalPrice / 100 ) + ".");
+};
+randomModel.printInfo(); 
